24f1a9e676e30abd1d557b7a6857ee45f9a8054d
noppefoxwolf <noppelabs@gmail.com>
2017-08-03 23:46:52 +0900

    support actual time scale and fps.

--- SCNVideoWriter/Classes/SCNVideoWriter.swift
+++ SCNVideoWriter/Classes/SCNVideoWriter.swift
@@ -20,20 +20,18 @@ public class SCNVideoWriter {
   private let input: AVAssetWriterInput
   private let pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor
   private let renderer: SCNRenderer
-  private var frameInfo: FrameInfo
   private let options: Options
   
   private let frameQueue = DispatchQueue(label: "com.noppelabs.SCNVideoWriter.frameQueue")
   private let renderQueue = DispatchQueue(label: "com.noppelabs.SCNVideoWriter.RenderQueue")
   private let renderSemaphore = DispatchSemaphore(value: 3)
   private var displayLink: CADisplayLink? = nil
-  private var currentTime: CMTime = kCMTimeZero
+  private var currentTime: CFTimeInterval = 0.0
   
   private var finishedCompletionHandler: ((_ url: URL) -> Void)? = nil
   
   public init?(scene: SCNScene, options: Options = .default) throws {
     self.options = options
-    self.frameInfo = FrameInfo(with: options.fps)
     self.renderer = SCNRenderer(device: nil, options: nil)
     renderer.scene = scene
     
@@ -74,19 +72,14 @@ public class SCNVideoWriter {
   }
   
   private func startDisplayLink() {
+    currentTime = 0.0
     displayLink = CADisplayLink(target: self, selector: #selector(updateDisplayLink))
+    displayLink?.preferredFramesPerSecond = options.fps
     displayLink?.add(to: .main, forMode: .commonModes)
   }
   
   @objc private func updateDisplayLink() {
-    guard let link = displayLink else { return }
-    currentTime = CMTimeMake(Int64(link.duration) * 600, 600)
-  }
-  
-  private func startInputPipeline() {
-    writer.startWriting()
-    writer.startSession(atSourceTime: kCMTimeZero)
-    input.requestMediaDataWhenReady(on: frameQueue) { [weak self] in
+    frameQueue.async { [weak self] in
       guard let input = self?.input, input.isReadyForMoreMediaData else { return }
       guard let pool = self?.pixelBufferAdaptor.pixelBufferPool else { return }
       guard let size = self?.options.videoSize else { return }
@@ -94,24 +87,24 @@ public class SCNVideoWriter {
     }
   }
   
+  private func startInputPipeline() {
+    writer.startWriting()
+    writer.startSession(atSourceTime: kCMTimeZero)
+    input.requestMediaDataWhenReady(on: frameQueue, using: {})
+  }
+  
   private func renderSnapshot(with pool: CVPixelBufferPool, video size: CGSize) {
-    // TODO: presentation timeをちゃんとした値にする
-    
-    let image = renderer.snapshot(atTime: displayLink!.duration * 1000,
+    guard let link = displayLink else { return }
+    guard link.preferredFramesPerSecond > 0 else { return }
+    currentTime += link.targetTimestamp - link.timestamp
+    let image = renderer.snapshot(atTime: currentTime,
                                   with: size,
                                   antialiasingMode: SCNAntialiasingMode.multisampling4X)
     let pixelBuffer = PixelBufferFactory.make(withSize: size, fromImage: image, usingBufferPool: pool)
-    pixelBufferAdaptor.append(pixelBuffer, withPresentationTime: CMTimeMake(Int64(displayLink!.duration * 1000.0) * 600, 600))
-    print(frameInfo.snapshotTime, frameInfo.presentationTime, displayLink?.timestamp, displayLink?.targetTimestamp, displayLink?.duration)
-    frameInfo.incrementFrame()
-    
-//    let image = renderer.snapshot(atTime: frameInfo.snapshotTime,
-//                                  with: size,
-//                                  antialiasingMode: SCNAntialiasingMode.multisampling4X)
-//    let pixelBuffer = PixelBufferFactory.make(withSize: size, fromImage: image, usingBufferPool: pool)
-//    pixelBufferAdaptor.append(pixelBuffer, withPresentationTime: frameInfo.presentationTime)
-//    frameInfo.incrementFrame()
-//    print(frameInfo.snapshotTime, frameInfo.presentationTime, displayLink?.timestamp, displayLink?.targetTimestamp, displayLink?.duration)
+    let timeScale: Int32 = 600
+    let value = currentTime * CFTimeInterval(timeScale)
+    let pt = CMTimeMake(Int64(value), timeScale)
+    pixelBufferAdaptor.append(pixelBuffer, withPresentationTime: pt)
   }
   
   private func stopDisplayLink() {
@@ -121,37 +114,6 @@ public class SCNVideoWriter {
 }
 
 extension SCNVideoWriter {
-  struct FrameInfo {
-    init(with fps: Int = 60) {
-      self.fps = fps
-    }
-    
-    private let timescale: Float = 600
-    private var frameNumber: Int = 0
-    private var fps: Int
-    
-    private var intervalDuration: CFTimeInterval {
-      return CFTimeInterval(1.0 / Double(fps))
-    }
-    
-    private var frameDuration: CMTime {
-      let kTimescale: Int32 = Int32(timescale)
-      return CMTimeMake(Int64( floor(timescale / Float(fps)) ), kTimescale)
-    }
-    
-    var snapshotTime: CFTimeInterval {
-      return CFTimeInterval(intervalDuration * CFTimeInterval(frameNumber))
-    }
-    
-    var presentationTime: CMTime {
-      return CMTimeMultiply(frameDuration, Int32(frameNumber))
-    }
-    
-    mutating func incrementFrame() {
-      frameNumber += 1
-    }
-  }
-  
   public struct Options {
     public var videoSize: CGSize
     public var fps: Int

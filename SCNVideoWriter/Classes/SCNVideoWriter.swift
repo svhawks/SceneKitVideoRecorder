415ef445c3bd4dabfd1d2d93d5abd5e3447dac7b
Omer Karisman <ok@okaris.com>
2017-08-29 19:23:56 +0300

    Remove SCNRenderer. Use CAMetalLayer insted.
    This way capturing the current frame is much faster and it oncludes animations too.

--- SCNVideoWriter/Classes/SCNVideoWriter.swift
+++ SCNVideoWriter/Classes/SCNVideoWriter.swift
@@ -9,36 +9,46 @@ import UIKit
 import SceneKit
 import ARKit
 import AVFoundation
+import CoreImage
 
 public class SCNVideoWriter {
   private let writer: AVAssetWriter
   private let input: AVAssetWriterInput
   private let pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor
-  private let renderer: SCNRenderer
   private let options: Options
-  
+
   private let frameQueue = DispatchQueue(label: "com.noppelabs.SCNVideoWriter.frameQueue")
   private static let renderQueue = DispatchQueue(label: "com.noppelabs.SCNVideoWriter.renderQueue")
   private static let renderSemaphore = DispatchSemaphore(value: 3)
   private var displayLink: CADisplayLink? = nil
   private var initialTime: CFTimeInterval = 0.0
   private var currentTime: CFTimeInterval = 0.0
-  
+
+  private var sceneView: SCNView
+  private var metalLayer: CAMetalLayer
+
   public var updateFrameHandler: ((_ image: UIImage, _ time: CMTime) -> Void)? = nil
   private var finishedCompletionHandler: ((_ url: URL) -> Void)? = nil
-  
+  private let context:CIContext
+
   @available(iOS 11.0, *)
   public convenience init?(withARSCNView view: ARSCNView, options: Options = .default) throws {
     var options = options
     options.renderSize = CGSize(width: view.bounds.width * view.contentScaleFactor, height: view.bounds.height * view.contentScaleFactor)
-    try self.init(scene: view.scene, options: options)
+    try self.init(scene: view, options: options)
   }
-  
-  public init?(scene: SCNScene, options: Options = .default) throws {
+
+  public init?(scene: SCNView, options: Options = .default) throws {
+
+    self.sceneView = scene
+
+    self.context = CIContext.init(mtlDevice: MTLCreateSystemDefaultDevice()!)
+
+    self.metalLayer = (sceneView.layer as? CAMetalLayer)!
+    self.metalLayer.framebufferOnly = false
+
     self.options = options
-    self.renderer = SCNRenderer(device: nil, options: nil)
-    renderer.scene = scene
-    
+
     self.writer = try AVAssetWriter(outputURL: options.outputUrl,
                                     fileType: options.fileType)
     self.input = AVAssetWriterInput(mediaType: AVMediaTypeVideo,
@@ -47,14 +57,14 @@ public class SCNVideoWriter {
                                                                    sourcePixelBufferAttributes: options.sourcePixelBufferAttributes)
     prepare(with: options)
   }
-  
+
   private func prepare(with options: Options) {
     if options.deleteFileIfExists {
       FileController.delete(file: options.outputUrl)
     }
     writer.add(input)
   }
-  
+
   public func startWriting() {
     SCNVideoWriter.renderQueue.async { [weak self] in
       SCNVideoWriter.renderSemaphore.wait()
@@ -62,7 +72,7 @@ public class SCNVideoWriter {
       self?.startInputPipeline()
     }
   }
-  
+
   public func finishWriting(completionHandler: (@escaping (_ url: URL) -> Void)) {
     let outputUrl = options.outputUrl
     input.markAsFinished()
@@ -72,7 +82,7 @@ public class SCNVideoWriter {
       SCNVideoWriter.renderSemaphore.signal()
     })
   }
-  
+
   private func startDisplayLink() {
     currentTime = 0.0
     initialTime = CFAbsoluteTimeGetCurrent()
@@ -80,7 +90,7 @@ public class SCNVideoWriter {
     displayLink?.preferredFramesPerSecond = options.fps
     displayLink?.add(to: .main, forMode: .commonModes)
   }
-  
+
   @objc private func updateDisplayLink() {
     frameQueue.async { [weak self] in
       guard let input = self?.input, input.isReadyForMoreMediaData else { return }
@@ -90,30 +100,44 @@ public class SCNVideoWriter {
       self?.renderSnapshot(with: pool, renderSize: renderSize, videoSize: videoSize)
     }
   }
-  
+
   private func startInputPipeline() {
     writer.startWriting()
     writer.startSession(atSourceTime: kCMTimeZero)
     input.requestMediaDataWhenReady(on: frameQueue, using: {})
   }
-  
+
   private func renderSnapshot(with pool: CVPixelBufferPool, renderSize: CGSize, videoSize: CGSize) {
     autoreleasepool {
+
+      let texture = metalLayer.nextDrawable()?.texture
+      let ciimage = CIImage(mtlTexture: texture!, options: nil)
+      let image = convert(ciImage: ciimage!)
+
       currentTime = CFAbsoluteTimeGetCurrent() - initialTime
-      let image = renderer.snapshot(atTime: currentTime, with: renderSize, antialiasingMode: .multisampling4X)
-      guard let croppedImage = image.fill(at: videoSize) else { return }
-      guard let pixelBuffer = PixelBufferFactory.make(with: videoSize, from: croppedImage, usingBuffer: pool) else { return }
+
+      let actualFramesPerSecond = 1 / ((displayLink?.targetTimestamp)! - (displayLink?.timestamp)!)
+      //`guard let croppedImage = image.fill(at: videoSize) else { return }
+      guard let pixelBuffer = PixelBufferFactory.make(with: videoSize, from: image, usingBuffer: pool) else { return }
       let value: Int64 = Int64(currentTime * CFTimeInterval(options.timeScale))
       let presentationTime = CMTimeMake(value, options.timeScale)
       pixelBufferAdaptor.append(pixelBuffer, withPresentationTime: presentationTime)
-      updateFrameHandler?(croppedImage, presentationTime)
+      updateFrameHandler?(image, presentationTime)
     }
   }
-  
+
+  func convert(ciImage:CIImage) -> UIImage
+  {
+    var flippedImage: CIImage = ciImage.applying(CGAffineTransform(scaleX: 1, y: -1))
+    flippedImage = flippedImage.applying(CGAffineTransform(translationX: 0, y: ciImage.extent.size.height))
+    let cgImage:CGImage = context.createCGImage(flippedImage, from: flippedImage.extent)!
+    let image:UIImage = UIImage.init(cgImage: cgImage)
+    return image
+  }
+
   private func stopDisplayLink() {
     displayLink?.invalidate()
     displayLink = nil
   }
 }
-
 

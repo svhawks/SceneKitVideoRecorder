7d6844ac36140b354e56ac4d68a29d69ecd24842
Omer Karisman <ok@okaris.com>
2017-09-04 21:02:27 +0300

    Use CIContext powered with Metal Device

--- SceneKitVideoRecorder/Classes/PixelBufferFactory.swift
+++ SceneKitVideoRecorder/Classes/PixelBufferFactory.swift
@@ -9,44 +9,45 @@
   }
   //Metal does not work in simulator :(
 #else
-import UIKit
+  import UIKit
 
-struct PixelBufferFactory {
+  struct PixelBufferFactory {
 
-  static let context = CIContext(options: nil)
+    static let context = CIContext(mtlDevice: MTLCreateSystemDefaultDevice()!)
 
-  static func make(with metalLayer: CAMetalLayer, usingBuffer pool: CVPixelBufferPool) -> (CVPixelBuffer?, UIImage) {
+    static func make(with metalLayer: CAMetalLayer, usingBuffer pool: CVPixelBufferPool) -> (CVPixelBuffer?, UIImage) {
 
-    let currentDrawable = metalLayer.nextDrawable()
-    let destinationTexture = currentDrawable?.texture
+      let currentDrawable = metalLayer.nextDrawable()
+      let destinationTexture = currentDrawable?.texture
 
-    var pixelBuffer: CVPixelBuffer?
-    let status = CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, pool, &pixelBuffer)
-    if let pixelBuffer = pixelBuffer {
-      CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags.init(rawValue: 0))
-      let region = MTLRegionMake2D(0, 0, Int((currentDrawable?.layer.drawableSize.width)!), Int((currentDrawable?.layer.drawableSize.height)!))
+      var pixelBuffer: CVPixelBuffer?
+      let status = CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, pool, &pixelBuffer)
+      if let pixelBuffer = pixelBuffer {
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags.init(rawValue: 0))
+        let region = MTLRegionMake2D(0, 0, Int((currentDrawable?.layer.drawableSize.width)!), Int((currentDrawable?.layer.drawableSize.height)!))
 
-      let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
 
-      let tempBuffer = CVPixelBufferGetBaseAddress(pixelBuffer)
-      destinationTexture?.getBytes(tempBuffer!, bytesPerRow: Int(bytesPerRow), from: region, mipmapLevel: 0)
-
-      let image = imageFromCVPixelBuffer(buffer: pixelBuffer)
-      CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags.init(rawValue: 0))
-      return (pixelBuffer, image)
+        let tempBuffer = CVPixelBufferGetBaseAddress(pixelBuffer)
+        destinationTexture?.getBytes(tempBuffer!, bytesPerRow: Int(bytesPerRow), from: region, mipmapLevel: 0)
+        
+        let image = imageFromCVPixelBuffer(buffer: pixelBuffer)
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags.init(rawValue: 0))
+        return (pixelBuffer, image)
+      }
+      return (nil, UIImage())
     }
-    return (nil, UIImage())
+
+    static func imageFromCVPixelBuffer(buffer: CVPixelBuffer) -> UIImage {
+
+      let ciimage = CIImage(cvPixelBuffer: buffer)
+      let cgimgage = context.createCGImage(ciimage, from: CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(buffer), height: CVPixelBufferGetHeight(buffer)))
+
+      let uiimage = UIImage(cgImage: cgimgage!)
+
+      return uiimage
+    }
+
   }
-
-  static func imageFromCVPixelBuffer(buffer: CVPixelBuffer) -> UIImage {
-
-    let ciimage = CIImage(cvPixelBuffer: buffer)
-    let cgimgage = context.createCGImage(ciimage, from: CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(buffer), height: CVPixelBufferGetHeight(buffer)))
-
-    let uiimage = UIImage(cgImage: cgimgage!)
-
-    return uiimage
-  }
-
-}
 #endif
+

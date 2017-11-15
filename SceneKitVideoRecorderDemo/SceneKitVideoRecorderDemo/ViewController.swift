7cfab817663e2c832ec644ea539f7b4013c111eb
Halil İbrahim Kayım <halil@mojilala.com>
2017-11-16 01:08:59 +0300

    Update demo project

--- SceneKitVideoRecorderDemo/SceneKitVideoRecorderDemo/ViewController.swift
+++ SceneKitVideoRecorderDemo/SceneKitVideoRecorderDemo/ViewController.swift
@@ -48,7 +48,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
   @IBAction func startRecording (sender: UIButton) {
     sender.backgroundColor = .red
-    self.recorder?.setupAudio()
     self.recorder?.startWriting()
   }
 
